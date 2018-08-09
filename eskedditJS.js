ad98c8f5c7ea6939acc5b32b3001546a9979a226
3XZACT <42225521+3XZACT@users.noreply.github.com>
2018-08-09 11:30:44 +1000

    Add files via upload

---
+++ eskedditJS.js
@@ -0,0 +1,21 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+}
+
+function changeimg() {
+	document.winklogo.src='eskedditlogowinking.png';
+}
+
+function revertimg() {
+	document.revertimg.src='eskedditlogo (3).png'
+}
